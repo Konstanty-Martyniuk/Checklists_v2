752a186661f995da71b5ee77d1cd5a1e2257950e
Konstanty Martyniuk <constantin.radecky@gmail.com>
2018-12-04 14:02:32 +0100

    Initial Commit

---
+++ Checklists_v2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Checklists_v2
+//
+//  Created by Konstanty on 04/12/2018.
+//  Copyright © 2018 Konstanty. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
