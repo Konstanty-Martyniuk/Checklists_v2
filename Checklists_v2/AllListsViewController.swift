889bd8bbd2cb9852ce3e86cecaa9574f747cfd99
Konstanty Martyniuk <constantin.radecky@gmail.com>
2018-12-26 23:47:23 +0100

    finished ch29

--- Checklists_v2/AllListsViewController.swift
+++ Checklists_v2/AllListsViewController.swift
@@ -8,8 +8,7 @@
 
 import UIKit
 
-class AllListsViewController: UITableViewController {
-    
+class AllListsViewController: UITableViewController, ListDetailViewControllerDelegate {
     var lists = [Checklist]()
     
     override func viewDidLoad() {
@@ -21,30 +20,13 @@ class AllListsViewController: UITableViewController {
         
         list = Checklist(name: "Groceries")
         lists.append(list)
-            
+        
         list = Checklist(name: "Cool Apps")
         lists.append(list)
         
         list = Checklist(name: "To Do")
         lists.append(list)
         
-    }
-
-    // MARK: - Table view data source
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return lists.count
-    }
-
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = makeCell(for: tableView)
-        let checklist = lists[indexPath.row]
-        cell.textLabel!.text = checklist.name
-        cell.accessoryType = .detailDisclosureButton
-        
-        return cell
     }
     
     func makeCell(for tableView: UITableView) -> UITableViewCell {
@@ -55,7 +37,25 @@ class AllListsViewController: UITableViewController {
             return UITableViewCell(style: .default, reuseIdentifier: cellIdentifier)
         }
     }
-
+    
+    // MARK: - Table view data source
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return lists.count
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = makeCell(for: tableView)
+        //update cell info
+        let checklist = lists[indexPath.row]
+        cell.textLabel!.text = checklist.name
+        cell.accessoryType = .detailDisclosureButton
+        
+        return cell
+    }
+    
     // MARK: - TableView delegates
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
@@ -63,13 +63,56 @@ class AllListsViewController: UITableViewController {
         performSegue(withIdentifier: "ShowChecklist", sender: checklist)
     }
     
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        lists.remove(at: indexPath.row)
+        
+        let indexPaths = [indexPath]
+        tableView.deleteRows(at: indexPaths, with: .automatic)
+    }
+    
+    override func tableView(_ tableView: UITableView, accessoryButtonTappedForRowWith indexPath: IndexPath) {
+        let controller = storyboard!.instantiateViewController(withIdentifier: "ListDetailViewController") as! ListDetailViewController
+        controller.delegate = self
+        let checklist = lists[indexPath.row]
+        controller.checklistToEdit = checklist
+        navigationController?.pushViewController(controller, animated: true)
+    }
+    
+    // MARK:- Navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "ShowChecklist" {
             let controller = segue.destination as! ChecklistViewController
-            controller.checklist = sender as! Checklist
+            controller.checklist = (sender as! Checklist)
+        } else if segue.identifier == "AddChecklist" {
+            let controller = segue.destination as! ListDetailViewController
+            controller.delegate = self
         }
     }
     
-    // MARK: - Navigation
-
+    // MARK:- List Detail View Controller Delegates
+    
+    func listDetailViewControllerDidCancel(_ controller: ListDetailViewController) {
+        navigationController?.popViewController(animated: true)
+    }
+    
+    func listDetailViewController(_ controller: ListDetailViewController, didFinishAdding checklist: Checklist) {
+        let newRowIndex = lists.count
+        lists.append(checklist)
+        
+        let indexPath = IndexPath(row: newRowIndex, section: 0)
+        let indexPaths = [indexPath]
+        tableView.insertRows(at: indexPaths, with: .automatic)
+        
+        navigationController?.popViewController(animated: true)
+    }
+    
+    func listDetailViewController(_ controller: ListDetailViewController, didFinishEditing checklist: Checklist) {
+        if let index = lists.index(of: checklist) {
+            let indexPath = IndexPath(row: index, section: 0)
+            if let cell = tableView.cellForRow(at: indexPath) {
+                cell.textLabel!.text = checklist.name
+            }
+        }
+        navigationController?.popViewController(animated: true)
+    }
 }
