5d57ee28a4c9146c43de5aa1eaab1c3a3ed0a435
Konstanty Martyniuk <constantin.radecky@gmail.com>
2018-12-28 19:06:08 +0100

    DataModel
    moving save/load func

---
+++ Checklists_v2/DataModel.swift
@@ -0,0 +1,12 @@
+//
+//  DataModel.swift
+//  Checklists_v2
+//
+//  Created by Konstanty on 28/12/2018.
+//  Copyright © 2018 Konstanty. All rights reserved.
+//
+
+import Foundation
+class DataModel {
+    var lists = [Checklist]()
+}
