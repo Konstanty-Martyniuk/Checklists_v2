b8392a55d48c73dd54daa167ba63bed1fa99446f
Konstanty Martyniuk <constantin.radecky@gmail.com>
2019-01-05 14:09:32 +0100

    last update before the end - icons

--- Checklists_v2/Data Models/Checklist.swift
+++ Checklists_v2/Data Models/Checklist.swift
@@ -11,7 +11,7 @@ import UIKit
 class Checklist: NSObject, Codable {
     var name = ""
     var items = [ChecklistItem]()
-    
+    var iconName = "Appointments"
     init(name: String) {
         self.name = name
         super.init()
